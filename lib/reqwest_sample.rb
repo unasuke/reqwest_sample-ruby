d3539ae090f24974a95a3a6dfc607c5b0a11e57e
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-12-26 13:53:25 +0900

    Add Response#inspect

--- lib/reqwest_sample.rb
+++ lib/reqwest_sample.rb
@@ -5,5 +5,11 @@ require_relative "reqwest_sample/reqwest_sample"
 
 module ReqwestSample
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Response
+    def inspect
+      body_preview = body.length > 50 ? "#{body[0, 50]}..." : body
+      "#<#{self.class} status=#{status} version=#{version.inspect} headers={#{headers.size} entries} body=#{body_preview.inspect}>"
+    end
+  end
 end
