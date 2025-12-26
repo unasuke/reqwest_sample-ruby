97edae3e05118d0eae4b080e945f6c9b2a1dc4fc
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-12-26 12:13:20 +0900

    Set rustflags on extconf.rb also

--- ext/reqwest_sample/extconf.rb
+++ ext/reqwest_sample/extconf.rb
@@ -3,4 +3,6 @@
 require "mkmf"
 require "rb_sys/mkmf"
 
-create_rust_makefile("reqwest_sample/reqwest_sample")
+create_rust_makefile("reqwest_sample/reqwest_sample") do |r|
+  r.extra_rustflags = ["--cfg=reqwest_unstable"]
+end
